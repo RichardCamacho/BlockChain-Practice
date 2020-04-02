73b43e8c70dfa3ce8626adcd3a9bf67b1623e3e2
RKamacho <richar199724@hotmail.com>
2020-04-02 16:37:06 -0500

    Cambios de FrontEnd

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,68 @@
 import React, { Component } from 'react';
+import Web3 from 'web3';
 import logo from '../logo.png';
 import './App.css';
 
 class App extends Component {
+  
+  async componentWillMount(){//life cycle method
+
+    await this.cargarWeb3();
+    //console.log(window.web3);
+    await this.cargarDatosBlockchain();
+  }
+
+  async cargarWeb3(){
+    //Detectando MetaMask en el navegador
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);//se crea la conexión implementando web3
+      await window.ethereum.enable();
+
+    }
+    // otros navegadores
+    else if (window.web3) {
+        window.web3 = new Web3(window.web3.currentProvider);
+    }
+    // Non-dapp browsers...
+    else {
+        console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
+  }
+
+  async cargarDatosBlockchain(){
+    const web3 = window.web3;
+    //cargar cuenta
+    const accounts = await web3.eth.getAccounts();
+    //console.log(accounts);
+    this.setState({account: accounts[0]});
+
+  }
+  
+constructor(props){
+  super(props);
+  this.state = {
+    account: ''
+  }
+}
+
   render() {
     return (
       <div>
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
+            href=""
             target="_blank"
             rel="noopener noreferrer"
           >
-            Dapp University
+            Marketplace
           </a>
         </nav>
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
+                <p>{this.state.account}</p>
                 <a
                   href="http://www.dappuniversity.com/bootcamp"
                   target="_blank"
